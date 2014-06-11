d3fdffe337fec1cc08e00d8d50b0f52906c87479
Amy <amstarry@gmail.com>
2014-06-11 17:08:55 -0400

    Blog Text

--- Gemfile
+++ Gemfile
@@ -4,6 +4,7 @@ source 'http://rubygems.org'
 
 gem "middleman", "~> 3.3.2"
 gem "middleman-blog", "~> 3.5.3"
+gem "nokogiri"
 
 # For feed.xml.builder
 gem "builder", "~> 3.0"
--- config.rb
+++ config.rb
@@ -12,7 +12,7 @@ activate :blog do |blog|
   # Matcher for blog source files
   # blog.sources = "{year}-{month}-{day}-{title}.html"
   # blog.taglink = "tags/{tag}.html"
-  # blog.layout = "layout"
+  blog.layout = "blog_layout"
   # blog.summary_separator = /(READMORE)/
   # blog.summary_length = 250
   # blog.year_link = "{year}.html"
